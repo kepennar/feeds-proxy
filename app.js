61a5f98e9993a3fdf6c917826ee35b5b4da96059
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-07-20 14:52:23 +0200

    fix CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', true);
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 
